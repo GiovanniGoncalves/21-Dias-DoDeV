3550e23086a4aab4dfcab208dc89d77c14879add
Giovanni Gonçalves <giovanniglirio@gmail.com>
2023-12-07 18:13:40 -0300

    Estrutura do For (Loop)

---
+++ dia-6/index.js
@@ -0,0 +1,7 @@
+// APRENDENDO LOOP DE REPETIÇÃO * FOR *
+
+let tabuada = Number(prompt('Escolha a tabuada que deseja'))
+
+for(let contador = 0; contador <=10; contador++){
+    console.log(tabuada + " x " + contador + " = " + tabuada * contador)
+}
