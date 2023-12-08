bb68cffd1f35c9568f7332489b58aba2819ca5b6
Giovanni Gonçalves <giovanniglirio@gmail.com>
2023-12-08 15:28:28 -0300

    Aprendi Do While

---
+++ dia-7/index.js
@@ -0,0 +1,33 @@
+//WHILE - ENQUANTO
+// let condicao = true;
+// while(condicao === true){
+//     let idade = Number(prompt('Insira sua idade'))
+//     if(idade < 18){
+//         console.log('você é menor de idade')
+//         condicao = false;
+//     }
+// }
+
+// console.log('acabou o while')
+
+//OUTRA FORMA
+// let idade = 18;
+// while(idade >= 18){
+//     idade = Number(prompt('Insira sua idade'))
+//     console.log(idade)
+// }
+// console.log("você é menor de idade")
+
+
+
+
+// DO WHILE
+
+let idade = 17;
+
+do  {  
+     idade = Number(prompt('Insira sua idade'))
+     console.log(idade)
+} while(idade >= 18)
+
+     console.log("você é menor de idade")
