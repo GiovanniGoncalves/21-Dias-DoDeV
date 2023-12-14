a8127b5c9b8c6d1f7d6633053edbcf2388b86cf6
Giovanni Gonçalves <giovanniglirio@gmail.com>
2023-12-14 20:22:39 -0300

    Dia #10 - Resolvendo exercícios Array

---
+++ dia-10/index.js
@@ -0,0 +1,15 @@
+let nomes = []; // Criando uma array vazio.
+continuar = true; // Apenas para controlar quando eu quero sair ou não da array.
+let posicaoDoArray = 0; // Em que posição do Array eu vou adicionar o nome.
+
+while(continuar === true){
+    let nomeInserido = prompt('Insira um nome');
+    nomes[posicaoDoArray] = nomeInserido
+
+    let desejaContinuar = prompt('Insira 1 caso deseje adicionar um novo nome')
+    if(desejaContinuar != '1'){
+        continuar = false;
+        break;
+    }
+    posicaoDoArray++;
+}
