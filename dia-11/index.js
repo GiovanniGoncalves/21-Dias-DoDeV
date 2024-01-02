df5709c0735d2adb4d88e259f55181c553e37761
Giovanni Gonçalves <giovanniglirio@gmail.com>
2024-01-02 11:50:15 -0300

    Desafio 1 - Avaliacao da Turma

--- dia-11/index.js
+++ dia-11/index.js
@@ -12,4 +12,39 @@
 
 
 // 4 - No final exiba a nota de cada aluno, a soma das notas e a média geral da turma. Para calcular a média deve dividir a soma das notas pelo total de alunos.
-// TESTE
+// TESTE OS CÓDIGOS.
+
+
+// CRIANDO VARIÁVEIS
+
+let alunos = []
+let notas = []
+let continuar = true
+let contador = 0
+
+//CRIANDO LOOP
+while (continuar) {
+    let nome = prompt('Digite o nome do ' + (contador + 1) + 'º aluno')
+    let nota = parseInt(prompt('Digite a nota do aluno'))
+    alunos[contador] = nome
+    notas [contador] = nota
+    contador++
+    let resposta = prompt('Deseja inserir informações sobre outro aluni? (s/n)')
+    if (resposta == 'n')
+        continuar = false
+}
+
+//EXIBINDO INFORMAÇÕES
+console.log('Notas dos alunos: ')
+for (let i = 0; i < alunos.length; i++) {
+    console.log(alunos[i] + ' - ' + notas[i])
+}
+
+let someNotas = 0
+for (let i = 0; i < notas.length; i++){
+    someNotas += notas[i]
+}
+
+let media = someNotas / alunos.length
+console.log('A soma das notas foi: ' + someNotas)
+console.log('A média geral da turma foi: ' + media)
